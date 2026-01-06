f6baaed07b1d166e09e998e6a1b5cbb06d578e0a
hhadi <hazem.hadi@appliedtechnology.se>
2026-01-06 17:08:18 +0100

    added ex12

--- 0201p1.py
+++ 0201p1.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-print( df )
+df = pd.DataFrame({"Name": ["Jacob", "Emily", "Peter"], "Age": [22, 33, 34], "Score": [123, 223, 132]})
+df = df.set_index("Name")
+df = df[df["Age"] >= 25]
+
+df["Score"] = df["Score"].astype(float)
+print(df) 
