d1b5894babbbd6c14d7f1f5b7356e61159f29342
hhadi <hazem.hadi@appliedtechnology.se>
2026-01-06 19:29:30 +0100

    finish with 9

--- 0201p1.py
+++ 0201p1.py
@@ -14,18 +14,40 @@ df2 = df.groupby("Age")["Score"].mean()
 mean_score = df["Score"].mean()
 df = df.fillna(mean_score)
 #P2
-#1
-df3 = pd.DataFrame({"Name":["Melina", "Sara", "Oscar", "Driton", "Potato", "Karin"], "Age":[23, 25, 44, 34, 53, 52], "Occupation": ["Welder", "Carpenter", "Engineer", "Doctor", "Service", "COO"]})
-df5 = pd.concat([df, df3], axis = 0)
-#2
-id_df1 = pd.DataFrame({"ID": [1, 2, 3], "Name":["Adam", "Emmelie", "Julia"], "Age": [32, 22, 23]})
-id_df2 = pd.DataFrame({"ID":[1, 2, 3], "Name": ["Karolin", "Peter", "Charles"], "Age": [31, 25, 29]})
+#6
+df3 = pd.DataFrame({"Name":["Melina", "Sara", "Oscar", "Driton", "Potato", "Karin"], 
+                    "Age":[23, 25, 44, 34, 53, 52], 
+                    "Occupation": ["Welder", "Carpenter", "Engineer", "Doctor", "Service", "COO"]})
 
-id_merge_df = pd.merge(id_df1, id_df2, on='ID')
-#print(df) 
+df5 = pd.concat([df, df3], axis = 0)
+#7
+id_df1 = pd.DataFrame({"ID": [1, 2, 3], 
+                       "Name":["Adam", "Emmelie", "Julia"], 
+                       "Age": [32, 22, 23]})
+
+id_df2 = pd.DataFrame({"ID":[1, 2, 3], 
+                       "Name": ["Karolin", "Peter", "Charles"], 
+                       "Age": [31, 25, 29]})
+
+id_merge_df = pd.merge(id_df1, id_df2, on='ID', how="outer")
+
+#8
+df_datetime = pd.DataFrame({"Date":['2002-02-03', '2033-12-31', '2012-05-04']})
+df_datetime["Date"] = pd.to_datetime(df_datetime["Date"])
+#9
+df_eventtime = df_datetime.rename(columns={"Date":"EventTime"})
+
+#10
+
+df_unique = pd.DataFrame({"Values": [1, 2, 1, 1 , 3, 4, 4 , 4]})
+
+print(df_unique)
+df_unique = df_unique.drop_duplicates()
+print(df_unique)
+#print(df)  
 #print(df3)
 #print(df5)
-
-print(id_df1)
-print(id_df2)
-print(id_merge_df)
+#print(id_df1)
+#print(id_df2)
+#print(id_merge_df)
+#print(df_eventtime)
